dc549aa7ad51d31f15f5353eb6fd7811a0126436
Ashish Khawla <khawla@fanaticcoders.com>
2026-03-06 14:53:24 +0530

    Revert "feat: add useActivityAddOnsAdmin hook for activity-type addons"
    This reverts commit dc708f60fafcba3bfaa0d174f10a7d4c2f7d353f.

--- src/hooks/api/admin/addon.js
+++ src/hooks/api/admin/addon.js
@@ -11,21 +11,3 @@ export function useAddOnOptionsAdmin() {
     mutate,
   };
 }
-
-/**
- * Get AddOn Options filtered by type='activity' for Activity forms
- * @returns {SWRResponse} SWR response with activity-type addons
- */
-export function useActivityAddOnsAdmin() {
-  const { data, error, isValidating, isLoading, mutate } = useSWR(
-    '/api/admin/addons/addondropdown?type=activity',
-    fetcher
-  );
-  return {
-    data,
-    isLoading,
-    isValidating,
-    error,
-    mutate,
-  };
-}
